4563f23db763535b53a7101c4d194011dfcd22c3
maasilliy <58572308+maasilliy@users.noreply.github.com>
2021-09-05 13:33:23 +0200

    new

---
+++ app/src/main/java/com/example/test/faceDetection/FaceDetectionProcess.kt
@@ -0,0 +1,60 @@
+package com.example.test.faceDetection
+
+import android.graphics.Rect
+import android.util.Log
+import com.example.test.camerax.BaseImage
+import com.example.test.camerax.Graphic
+import com.google.android.gms.tasks.Task
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.face.Face
+import com.google.mlkit.vision.face.FaceDetection
+import com.google.mlkit.vision.face.FaceDetectorOptions
+import java.io.IOException
+
+class FaceDetectionProcess(private val view: Graphic) :
+    BaseImage<List<Face>>() {
+
+    private val realTimeOpts = FaceDetectorOptions.Builder()
+        .setPerformanceMode(FaceDetectorOptions.PERFORMANCE_MODE_FAST)
+        .setContourMode(FaceDetectorOptions.CONTOUR_MODE_NONE)
+        .build()
+
+    private val detector = FaceDetection.getClient(realTimeOpts)
+
+    override val graphicOverlay: Graphic
+        get() = view
+
+    override fun detectInImage(image: InputImage): Task<List<Face>> {
+        return detector.process(image)
+    }
+
+    override fun stop() {
+        try {
+            detector.close()
+        } catch (e: IOException) {
+            Log.e(TAG, "Exception thrown while trying to close Face Detector: $e")
+        }
+    }
+
+    override fun onSuccess(
+        results: List<Face>,
+        graphicOverlay: Graphic,
+        rect: Rect
+    ) {
+        graphicOverlay.clear()
+        results.forEach {
+            val faceGraphic = FaceContour(graphicOverlay, it, rect)
+            graphicOverlay.add(faceGraphic)
+        }
+        graphicOverlay.postInvalidate()
+    }
+
+    override fun onFailure(e: Exception) {
+        Log.w(TAG, "Face Detector failed.$e")
+    }
+
+    companion object {
+        private const val TAG = "FaceDetectorProcessor"
+    }
+
+}
